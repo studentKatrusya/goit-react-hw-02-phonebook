e45c1de0e6fe8029c6c89abd0233246578118949
Katrusya Panchenko <katrusya.pan4enko@gmail.com>
2022-02-10 12:22:36 +0200

    start file

---
+++ src/components/App.jsx
@@ -0,0 +1,81 @@
+import s from './App.module.css';
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+  render() {
+    const { contacts } = this.state;
+    console.log(contacts);
+    return (
+      <>
+        <h2 className={s.title}>Phonebook</h2>
+        <form className={s.form}>
+          <label className={s.label}>Name</label>
+          <input
+            className={s.input}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <label className={s.label}>Number</label>
+          <input
+            className={s.input}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+          <button className={s.btn} type="button">
+            Add contact
+          </button>
+        </form>
+        <h3 className={s.title}>Contacts</h3>
+        <div>
+          <label className={s.label}>Find contact by name</label>
+          <input
+            className={s.input}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          ></input>
+        </div>
+
+        <ul>
+          <li>{/* {contacts}:{contacts} */}</li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default App;
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         textTransform: 'uppercase',
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
